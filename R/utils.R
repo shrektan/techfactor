0cf34fb9b5027c2748c7612c74fe6d73ca6cdd16
shrektan <shrektan@126.com>
2018-05-03 09:34:30 +0800

    decrease the dependencies

--- R/utils.R
+++ R/utils.R
@@ -5,12 +5,7 @@ NULL
 #' @import data.table
 #' @import assertthat
 #' @importFrom magrittr %>%
-#' @export %>%
-#' @importFrom purrr %||%
-#' @export %||%
-#' @importFrom GCAMCPUB setDtTz setDtEnc is_unique_dt is_pk_dt to_date na_fill
-#' @importFrom GCAMCPUB saveDtRds readDtRds writeDtFeather readDtFeather time2char timeStamp
-#' @importFrom GCAMCPUB from_to as_from_to is_from_to na_fill %==% %!=% %safediv% str_left str_right
-#' @importFrom GCAMCPUB log_debug log_info log_warn log_error log_fatal logging
-#' @export from_to as_from_to is_from_to na_fill to_date is_pk_dt %==% %!=% %safediv%
-NULL
+
+
+#' @export
+`%>%`
