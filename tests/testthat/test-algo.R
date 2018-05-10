4f50749b347b9efacd4707c8e376aa18fa673b63
shrektan <shrektan@126.com>
2018-05-10 15:16:25 +0800

    improve tests and add tests for tsmin and tsmax

--- tests/testthat/test-algo.R
+++ tests/testthat/test-algo.R
@@ -15,10 +15,11 @@ test_that("na_vector", {
 
 
 test_that("delta", {
+  expect_equal(tf_delta(c(3, 5, NA, NA, 3, 4, NA)), c(2, NA, NA, NA, 1, NA))
   expect_equal(tf_delta(c(3, 8, 20)), c(5, 12))
   expect_equal(tf_delta(c(3, 3)), 0)
-  expect_error(tf_delta(3), "x must have at least two elements.", fixed = TRUE)
-  expect_error(tf_delta(double()), "x must have at least two elements.", fixed = TRUE)
+  expect_error(tf_delta(3), "x must have at least 2 elements.", fixed = TRUE)
+  expect_error(tf_delta(double()), "x must have at least 2 elements.", fixed = TRUE)
 })
 
 test_that("rank", {
@@ -26,28 +27,44 @@ test_that("rank", {
   expect_equal(tf_rank(x), frank(x, ties.method = "min"))
   expect_equal(tf_rank(double()), double())
   expect_equal(tf_rank(20), 1)
+  expect_equal(tf_rank(c(1, 3, 5, NA, 7, 9)), rep(NA_real_, 6))
 })
 
 test_that("sum", {
   expect_equal(tf_sum(double()), 0)
   expect_equal(tf_sum(1.5), 1.5)
   expect_equal(tf_sum(c(0.3, 1.5)), 1.8)
+  expect_equal(tf_sum(c(0.3, 1.5, NA_real_)), NA_real_)
 })
 
 test_that("mean", {
-  expect_equal(tf_mean(double()), NA_real_)
+  expect_error(tf_mean(double()), "x must have at least 1 elements.", fixed = TRUE)
   expect_equal(tf_mean(1.5), 1.5)
+  expect_equal(tf_mean(c(1.5, NA_real_)), NA_real_)
   expect_equal(tf_mean(c(0.3, 1.5)), 0.9)
 })
 
 test_that("stdev", {
-  expect_equal(tf_stdev(double()), NA_real_)
-  expect_equal(tf_stdev(1.2), NA_real_)
+  expect_error(tf_stdev(double()), "x must have at least 2 elements.", fixed = TRUE)
+  expect_error(tf_stdev(1.2), "x must have at least 2 elements.", fixed = TRUE)
 
   x <- rnorm(10)
   expect_equal(tf_stdev(x), sd(x))
+  expect_equal(tf_stdev(c(NA, x)), NA_real_)
 })
 
 test_that("tsmin", {
-  tf_tsmin()
+  x <- rnorm(10)
+  expect_equal(tf_tsmin(x), min(x))
+  expect_equal(tf_tsmin(c(NA, x)), NA_real_)
+  expect_error(tf_tsmin(double()), "at least 1 elements")
+  expect_equal(tf_tsmin(1.2), 1.2)
+})
+
+test_that("tsmax", {
+  x <- rnorm(10)
+  expect_equal(tf_tsmax(x), max(x))
+  expect_equal(tf_tsmax(c(NA, x)), NA_real_)
+  expect_error(tf_tsmax(double()), "at least 1 elements")
+  expect_equal(tf_tsmax(1.2), 1.2)
 })
