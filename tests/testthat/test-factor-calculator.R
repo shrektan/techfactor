a7a884155226861bf7378b53ebd1b1d0d092b8db
shrektan <shrektan@126.com>
2018-05-11 14:39:07 +0800

    add factor calculator

---
+++ tests/testthat/test-factor-calculator.R
@@ -0,0 +1,22 @@
+context("test-factor-calculator.R")
+
+data("tf_quote")
+dt <- copy(tf_quote)
+qt <- tf_quotes_xptr(dt)
+
+test_that("tf_reg_factors", {
+  expect_is(tf_reg_factors(), "character")
+})
+
+test_that("all the factors can be run", {
+  factors <- tf_reg_factors()
+  from_to <- range(tail(dt$DATE, 10))
+  for (factor in factors) {
+    res <- tf_factor(qt, factor, from_to)
+    expect_is(res, "data.frame")
+    expect_equal(nrow(res), 10)
+    expect_equal(res$DATE, tail(dt$DATE, 10))
+    expect_true(all(is.finite(res$VALUE)))
+    expect_true(!any(is.nan(res$VALUE)))
+  }
+})
