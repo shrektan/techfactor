538a9546e0270cc1899b1fb72d53c036870cefc9
shrektan <shrektan@126.com>
2018-05-11 14:22:29 +0800

    add sample data

---
+++ data-raw/sample-data.R
@@ -0,0 +1,7 @@
+tf_quote <- data.table::fread(
+  "tests/testthat/quote-sample.csv",
+  colClasses = c("character", rep("double", 10))
+)
+tf_quote[, DATE := as.Date(DATE)]
+data.table::setkey(tf_quote, DATE)
+usethis::use_data(tf_quote, internal = FALSE)
