5b651593952e726b00a627546594c828ba6ffe12
shrektan <shrektan@126.com>
2018-05-11 15:11:02 +0800

    add tests to increase code coverage

--- tests/testthat/test-factor-calculator.R
+++ tests/testthat/test-factor-calculator.R
@@ -8,6 +8,14 @@ test_that("tf_reg_factors", {
   expect_is(tf_reg_factors(), "character")
 })
 
+
+test_that("fails for undefined factor names", {
+  expect_error(
+    tf_factor(qt, "#garbname#", anydate(c(20180101, 20180109))),
+    "factor #garbname# must be defined before using"
+  )
+})
+
 test_that("all the factors can be run", {
   factors <- tf_reg_factors()
   from_to <- range(tail(dt$DATE, 10))
