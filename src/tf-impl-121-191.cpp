634c9a3c98a3da1e0c4462f04230f3953a3591d2
shrektan <shrektan@126.com>
2018-05-14 17:45:48 +0800

    impl alpha122

--- src/tf-impl-121-191.cpp
+++ src/tf-impl-121-191.cpp
@@ -5,15 +5,29 @@ namespace alpha_impl
 {
 
 // ((RANK((VWAP -MIN(VWAP, 12)))^TSRANK(CORR(TSRANK(VWAP, 20), TSRANK(MEAN(VOLUME,60), 2), 18), 3)) * -1)
-Alpha_fun alpha121 = [](const Quotes& quotes) -> double {
-
-};
+// Alpha_fun alpha121 = [](const Quotes& quotes) -> double {
+//
+// };
 
 
 // (SMA(SMA(SMA(LOG(CLOSE),13,2),13,2),13,2)-DELAY(SMA(SMA(SMA(LOG(CLOSE),13,2),13,2),13,2),1))/
 // DELAY(SMA(SMA(SMA(LOG(CLOSE),13,2),13,2),13,2),1)
 Alpha_fun alpha122 = [](const Quotes& quotes) -> double {
-  return sma(log(quotes.ts_close(13)), 2);
+  auto tripple_sma = [&quotes](const int delay) {
+    Timeseries outer;
+    for (int i {12}; i >= 0; --i) {
+      Timeseries inner;
+      for (int j {12}; j >= 0; --j) {
+        inner.push_back(sma(log(quotes.ts_close(i + j + delay)), 2));
+      }
+      outer.push_back(sma(inner, 2));
+    }
+    return sma(outer, 2);
+  };
+  const double second = tripple_sma(1);
+  if (second == 0) return NA_REAL;
+  const double first = tripple_sma(0);
+  return first / second - 1.0;
 };
 
 
