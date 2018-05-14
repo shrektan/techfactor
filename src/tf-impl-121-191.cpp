817eb48ae0dd686aff915fca178c643dd8d423f5
shrektan <shrektan@126.com>
2018-05-14 17:21:17 +0800

    impl 122

--- src/tf-impl-121-191.cpp
+++ src/tf-impl-121-191.cpp
@@ -4,6 +4,19 @@
 namespace alpha_impl
 {
 
+// ((RANK((VWAP -MIN(VWAP, 12)))^TSRANK(CORR(TSRANK(VWAP, 20), TSRANK(MEAN(VOLUME,60), 2), 18), 3)) * -1)
+Alpha_fun alpha121 = [](const Quotes& quotes) -> double {
+
+};
+
+
+// (SMA(SMA(SMA(LOG(CLOSE),13,2),13,2),13,2)-DELAY(SMA(SMA(SMA(LOG(CLOSE),13,2),13,2),13,2),1))/
+// DELAY(SMA(SMA(SMA(LOG(CLOSE),13,2),13,2),13,2),1)
+Alpha_fun alpha122 = [](const Quotes& quotes) -> double {
+  return sma(log(quotes.ts_close(13)), 2);
+};
+
+
 Alpha_fun alpha149 = [](const Quotes& quotes) -> double {
   const auto dr = quotes.ts_close(252) / quotes.ts_close(252, 1) - 1.0;
   const auto bmk_dr = quotes.ts_bmk_close(252) / quotes.ts_bmk_close(252, 1) - 1.0;
