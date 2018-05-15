bdd66616deab65628b524a254010cb5f3a263fc6
Wang <nkwangpeng@126.com>
2018-05-15 15:10:07 +0800

    add factor alpha12- alpha28

--- src/tf-impl-001-060.cpp
+++ src/tf-impl-001-060.cpp
@@ -129,13 +129,167 @@ Alpha_fun alpha011 = [](const Quote& quote) -> double {
 
 
 // (RANK((OPEN -(SUM(VWAP, 10) / 10)))) * (-1 * (RANK(ABS((CLOSE -VWAP)))))
+Alpha_fun alpha012 = [](const Quote& quote) -> double {
+  return 1;
+};
 
+
+// (((HIGH * LOW)^0.5) -VWAP)
+Alpha_fun alpha013 = [](const Quote& quote) -> double {
+  return std::pow((quote.high() * quote.low()), 0.5) - quote.vwap();
+};
 
 
 // CLOSE-DELAY(CLOSE,5)
 Alpha_fun alpha014 = [](const Quote& quote) -> double {
   return quote.close() - quote.close(5);
 };
+
+
+// OPEN/DELAY(CLOSE,1)-1
+Alpha_fun alpha015 = [](const Quote& quote) -> double {
+  return quote.open() / quote.close(1) - 1.0;
+};
+
+
+// (-1 * TSMAX(RANK(CORR(RANK(VOLUME), RANK(VWAP), 5)), 5))
+Alpha_fun alpha016 = [](const Quote& quote) -> double {
+  return 1;
+};
+
+
+// RANK((VWAP -MAX(VWAP, 15)))^DELTA(CLOSE, 5)
+Alpha_fun alpha017 = [](const Quote& quote) -> double {
+  return 1;
+};
+
+
+// CLOSE/DELAY(CLOSE,5)
+Alpha_fun alpha018 = [](const Quote& quote) -> double {
+  return quote.close() / quote.close(5);
+};
+
+
+// (CLOSE<DELAY(CLOSE,5)?
+// (CLOSE-DELAY(CLOSE,5))/DELAY(CLOSE,5):(CLOSE=DELAY(CLOSE,5)?
+// 0:(CLOSE-DELAY(CLOSE,5))/CLOSE))
+Alpha_fun alpha019 = [](const Quote& quote) -> double {
+  if (quote.close() < quote.close(5)) {
+    return (quote.close() - quote.close(5)) / quote.close(5);
+  } else if (quote.close() == quote.close(5)) {
+    return 0.0;
+  } else {
+    return (quote.close() - quote.close(5)) / quote.close();
+  }
+};
+
+
+// (CLOSE-DELAY(CLOSE,6))/DELAY(CLOSE,6)*100
+Alpha_fun alpha020 = [](const Quote& quote) -> double {
+  return (quote.close() - quote.close(6)) / quote.close(6) * 100;
+};
+
+
+// REGBETA(MEAN(CLOSE,6),SEQUENCE(6))
+Alpha_fun alpha021 = [](const Quote& quote) -> double {
+  auto fun = [&quote](const int i) {
+    return mean(quote.ts_close(6, i));
+  };
+  Timeseries quote_mean = ts<double>(6, fun);
+  return regbeta(quote_mean, sequence(6));
+};
+
+
+// SMA(((CLOSE-MEAN(CLOSE,6))/MEAN(CLOSE,6)-DELAY((CLOSE-MEAN(CLOSE,6))/MEAN(CLOSE,6),3)),12,1)
+Alpha_fun alpha022 = [](const Quote& quote) -> double {
+  auto fun = [&quote](const int delay) {
+    double param1 = quote.close(delay) - mean(quote.ts_close(6, delay));
+    double param2 = quote.close(delay + 3) - mean(quote.ts_close(6, delay + 3));
+    return param1 - param2;
+  };
+  return sma(ts<double>(12, fun), 1);
+};
+
+
+// SMA((CLOSE>DELAY(CLOSE,1)? STD(CLOSE,20):0),20,1)/
+// (SMA((CLOSE>DELAY(CLOSE,1)?STD(CLOSE,20):0),20,1)+SMA((CLOSE<=DELAY(CLOSE,1)?STD(CLOSE,20):0),20,1))*
+// 100
+Alpha_fun alpha023 = [](const Quote& quote) -> double {
+  auto fun1 = [&quote](const int delay) {
+    if (quote.close(delay) > quote.close(delay + 1)) {
+      return stdev(quote.ts_close(20, delay));
+    } else {
+      return 0.0;
+    }
+  };
+  auto fun2 = [&quote](const int delay) {
+    if (quote.close(delay) <= quote.close(delay + 1)) {
+      return stdev(quote.ts_close(20, delay));
+    } else {
+      return 0.0;
+    }
+  };
+  double sma1 = sma(ts<double>(20, fun1), 1);
+  double sma2 = sma(ts<double>(20, fun2), 1);
+  return sma1 / (sma1 + sma2) * 100;
+};
+
+
+// SMA(CLOSE-DELAY(CLOSE,5),5,1)
+Alpha_fun alpha024 = [](const Quote& quote) -> double {
+  auto fun = [&quote](const int delay) {
+    return quote.close(delay) - mean(quote.ts_close(5, delay));
+  };
+  return sma(ts<double>(5, fun), 1);
+};
+
+
+// (-1 * RANK((DELTA(CLOSE, 7) * (1 -RANK(DECAYLINEAR((VOLUME / MEAN(VOLUME,20)), 9)))))) *
+// (1 + RANK(SUM(RET, 250)))
+Alpha_fun alpha025 = [](const Quote& quote) -> double {
+  return 1;
+};
+
+
+// ((SUM(CLOSE, 7) / 7) -CLOSE) + CORR(VWAP, DELAY(CLOSE, 5), 230)
+Alpha_fun alpha026 = [](const Quote& quote) -> double {
+  double rk_close_diff = mean(quote.ts_close(7)) - quote.close();
+  double rk_corr = corr(quote.ts_vwap(230), quote.ts_close(230, 5));
+  return rk_close_diff + rk_corr;
+};
+
+
+// WMA((CLOSE-DELAY(CLOSE,3))/DELAY(CLOSE,3)*100+(CLOSE-DELAY(CLOSE,6))/DELAY(CLOSE,6)*100,12)
+Alpha_fun alpha027 = [](const Quote& quote) -> double {
+  auto fun = [&quote](const int delay) {
+    double rk_close_ret_d3 = (quote.close(delay) - quote.close(delay + 3)) / quote.close(delay + 3);
+    double rk_close_ret_d6 = (quote.close(delay) - quote.close(delay + 6)) / quote.close(delay + 6);
+    return rk_close_ret_d3 * 100 + rk_close_ret_d6 * 100;
+  };
+  return wma(ts<double>(12, fun));
+};
+
+
+// 3*SMA((CLOSE-TSMIN(LOW,9))/(TSMAX(HIGH,9)-TSMIN(LOW,9))*100,3,1)-
+// 2*SMA(SMA((CLOSE-TSMIN(LOW,9))/(TSMAX(HIGH,9)-TSMAX(LOW,9))*100,3,1),3,1)
+Alpha_fun alpha028 = [](const Quote& quote) -> double {
+  auto fun1 = [&quote](const int delay1) {
+    double param1 = quote.close(delay1) - tsmin(quote.ts_low(9, delay1));
+    double param2 = tsmax(quote.ts_high(9, delay1)) - tsmin(quote.ts_low(9, delay1));
+    return param1 / param2 * 100;
+  };
+  auto fun2 = [&quote](const int delay1) {
+    auto fun3 = [&quote, delay1](const int delay2) {
+      double param1 = quote.close(delay1 + delay2) - tsmin(quote.ts_low(9, delay1 + delay2));
+      double param2 = tsmax(quote.ts_high(9, delay1 + delay2)) - tsmin(quote.ts_low(9, delay1 + delay2));
+      return param1 / param2 * 100;
+    };
+    return sma(ts<double>(3, fun3), 1);
+  };
+  return sma(ts<double>(3, fun1), 1) * 3 - sma(ts<double>(3, fun2), 1) * 2;
+};
+
+
 
 
 // COUNT(CLOSE>DELAY(CLOSE,1),12)/12*100
--- src/tf-impl.cpp
+++ src/tf-impl.cpp
@@ -16,7 +16,23 @@ extern Alpha_fun alpha008;
 extern Alpha_fun alpha009;
 extern Alpha_fun alpha010;
 extern Alpha_fun alpha011;
+extern Alpha_fun alpha012;
+extern Alpha_fun alpha013;
 extern Alpha_fun alpha014;
+extern Alpha_fun alpha015;
+extern Alpha_fun alpha016;
+extern Alpha_fun alpha017;
+extern Alpha_fun alpha018;
+extern Alpha_fun alpha019;
+extern Alpha_fun alpha020;
+extern Alpha_fun alpha021;
+extern Alpha_fun alpha022;
+extern Alpha_fun alpha023;
+extern Alpha_fun alpha024;
+extern Alpha_fun alpha025;
+extern Alpha_fun alpha026;
+extern Alpha_fun alpha027;
+extern Alpha_fun alpha028;
 extern Alpha_fun alpha053;
 extern Alpha_fun alpha149;
 }
@@ -41,7 +57,23 @@ std::map<std::string, Alpha_fun&> tf_caculators
   {"alpha009", alpha_impl::alpha009},
   {"alpha010", alpha_impl::alpha010},
   {"alpha011", alpha_impl::alpha011},
+  {"alpha012", alpha_impl::alpha012},
+  {"alpha013", alpha_impl::alpha013},
   {"alpha014", alpha_impl::alpha014},
+  {"alpha015", alpha_impl::alpha015},
+  {"alpha016", alpha_impl::alpha016},
+  {"alpha017", alpha_impl::alpha017},
+  {"alpha018", alpha_impl::alpha018},
+  {"alpha019", alpha_impl::alpha019},
+  {"alpha020", alpha_impl::alpha020},
+  {"alpha021", alpha_impl::alpha021},
+  {"alpha022", alpha_impl::alpha022},
+  {"alpha023", alpha_impl::alpha023},
+  {"alpha024", alpha_impl::alpha024},
+  {"alpha025", alpha_impl::alpha025},
+  {"alpha026", alpha_impl::alpha026},
+  {"alpha027", alpha_impl::alpha027},
+  {"alpha028", alpha_impl::alpha028},
   {"alpha053", alpha_impl::alpha053},
   {"alpha149", alpha_impl::alpha149}
 };
