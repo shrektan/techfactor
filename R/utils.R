35f50975459f3a5f9b08bf91b53bb244dd289f28
shrektan <shrektan@126.com>
2018-05-19 16:27:47 +0800

    improve the documents of the sample data

--- R/utils.R
+++ R/utils.R
@@ -19,10 +19,25 @@ anytime::anydate
 zoo::index
 
 
-#' The sample quote data used in GCAMCTF
+#' The sample quote data
 #'
-#' The data is a realistic China-A security quote of 81 days
-#' in total, from 2018-01-02 ~ 2018-05-07. The column `VWAP`
+#' We include some small chunck of sample data `tf_quote` and
+#' `tf_quotes` to illustrate what kind of data the package functions
+#' are expected and they are used in the tests as well.
+#'
+#' @details
+#'
+#' * `tf_quote` is a China-A security `000002.SZ` (Wanke-A)'s
+#' quote of 81 days in total, from 2018-01-02 ~ 2018-05-07.
+#'
+#' * `tf_quotes` is a list of quotes from 20140101 ~ 20180430,
+#' containing `300333.SZ`,`601158.SH`,`002788.SZ`,`603101.SH`,
+#' `600020.SH`,`601668.SH`,`600615.SH`,`002721.SZ`,`300517.SZ`,
+#' `601567.SH`,`603477.SH`,`002297.SZ`,`600537.SH`,`603906.SH`,
+#' `603183.SH`,`002884.SZ`,`300531.SZ`,`002641.SZ`,
+#' `002851.SZ`,`600719.SH`.
+#'
+#' * The column `VWAP`
 #' equals to `AMOUNT` / `VOLUME`. The columns `BMK_CLOSE` and
 #' `BMK_OPEN` is the close and open price data of the index
 #' _China-A All Shares_.
@@ -32,4 +47,12 @@ zoo::index
 #' @docType data
 #' @keywords data
 #' @usage data(tf_quote)
+#' @usage data(tf_quotes)
 NULL
+
+
+#' @rdname tf_quote
+#' @usage data(tf_quotes)
+"tf_quotes"
+
+
